d729d1366dbe6b9ef2a308eabd86aeeb1f82766c
LAPTOP-KLALN67F\Carlos <juan.romero@tesamerica.com.mx>
2022-11-25 00:19:21 -0600

    V1

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    const resultados = document.getElementById('resultado');
+    const btn_save = document.getElementById('btn_save'),
+        staticBackdrop = document.getElementById('staticBackdrop'),
+        modal = new bootstrap.Modal(staticBackdrop,{
+            backdrop: 'static'
+        }),
+        codigo = document.getElementById('codigo'),
+        marca = document.getElementById('marca'),
+        cantidad = document.getElementById('cantidad'),
+        ubicacion = document.getElementById('ubicacion'),
+        codigo_error = document.getElementById('codigo_error'),
+        marca_error = document.getElementById('marca_error'),
+        cantidad_error = document.getElementById('cantidad_error'),
+        ol_products = document.getElementById('ol_products'),
+        btn_delete = document.getElementById('btn_delete')
+        ;
+        
+    let products = [], id = '';
+    const renderProducts = ()=>{
+        ol_products.innerHTML = '';
+        products.forEach((element,i) => {
+            ol_products.innerHTML += `
+                <li class="list-group-item d-flex justify-content-between align-items-start" data-id="${i}">
+                      <div class="ms-2 me-auto">
+                        <div class="fw-bold">${element.code}</div>
+                        ${element.brand}
+                      </div>
+                      <span class="badge bg-primary rounded-pill">${element.quantity}</span>
+                </li>
+            `;
+        });
+    }
+
+    if(localStorage.getItem('products')){
+        products = JSON.parse(localStorage.getItem('products'));
+        renderProducts();
+    }
+
     Quagga.init({
         inputStream:{
             constraints:{
@@ -23,31 +59,95 @@ document.addEventListener("DOMContentLoaded", ()=>{
     });
 
     Quagga.onDetected((data)=>{
-        resultados.textContent = data.codeResult.code;
-        console.log(data)
+        staticBackdrop.setAttribute('data-id','insert');
+        btn_delete.classList.add('d-none');
+        modal.show();
+        codigo.value = data.codeResult.code;
     });
 
-    /* Quagga.onProcessed((result)=>{
-        let drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay;
+    btn_save.addEventListener('click', ()=>{
 
-        if(result){
-            if(result.boxes){
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-				result.boxes.filter(function (box) {
-					return box !== result.box;
-				}).forEach(function (box) {
-					Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-				});
+        if(validateInputs()){
+
+            if(staticBackdrop.getAttribute('data-id') === 'insert'){
+                products.push({
+                    'code': codigo.value,
+                    'brand': marca.value,
+                    'quantity': cantidad.value,
+                    'ubication': ubicacion.value
+                })
+            }else{
+                products[id] = {
+                    'code': codigo.value,
+                    'brand': marca.value,
+                    'quantity': cantidad.value,
+                    'ubication': ubicacion.value
+                };
             }
+    
+            cantidad.value = 1;
 
-            if (result.box) {
-				Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-			}
-
-			if (result.codeResult && result.codeResult.code) {
-				Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-			}
+            renderProducts();
+            localStorage.setItem('products', JSON.stringify(products));
+            modal.hide();
         }
-    }); */
-})
+    })
+
+    const validateInputs = ()=>{
+
+        let band = true;
+        
+        if(codigo.value === ''){
+            band = false;
+            codigo_error.innerText = 'El código es obligatorio';
+            codigo_error.classList.remove('d-none');
+        }else{
+            codigo_error.classList.add('d-none');
+        }
+
+        if(marca.value === ''){
+            band = false;
+            marca_error.innerText = 'El código es obligatorio';
+            marca_error.classList.remove('d-none');
+        }else{
+            marca_error.classList.add('d-none');
+        }
+
+        if(cantidad.value === ''){
+            band = false;
+            cantidad_error.innerText = 'El código es obligatorio';
+            cantidad_error.classList.remove('d-none');
+        }else{
+            cantidad_error.classList.add('d-none');
+        }
+
+        return band;
+    }
+
+    ol_products.addEventListener('click',(e)=>{
+        const li = e.target;
+        if(li.tagName === 'LI'){
+
+            id = li.getAttribute('data-id');
+            const product = products[id];
+
+            
+            staticBackdrop.setAttribute('data-id', 'update');
+            codigo.value = product.code;
+            marca.value = product.brand;
+            cantidad.value = product.quantity;
+            ubicacion.value = product.ubication;
+
+            btn_delete.classList.remove('d-none');
+            modal.show();
+        }
+    })
+
+    btn_delete.addEventListener('click', ()=>{
+        products.splice(id, 1);
+
+        renderProducts();
+        localStorage.setItem('products', JSON.stringify(products));
+        modal.hide();
+    })
+});
